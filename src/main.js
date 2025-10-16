cce2da88d4abfde8048e26e20d6c42b87cc71fe2
Jules Leo Reserva <jules.leo.reserva@gmail.com>
2025-10-16 17:31:21 +0800

    Make a "clickable" attribute for aframe-html-shader elements. Made the main-ui a clickable element.

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
-import 'aframe';
-import 'aframe-html-shader';
+import "aframe";
+import "aframe-html-shader";
+
+AFRAME.registerComponent("clickable", {
+  init: function () {
+    this.el.addEventListener("click", function (event) {
+      console.log("Entity clicked!", event.target);
+    });
+  },
+});
