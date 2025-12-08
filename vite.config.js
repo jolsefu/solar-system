ace5dbb5212ec0f928836765f9c4d06e0a10790b
Jules Leo Reserva <jules.leo.reserva@gmail.com>
2025-12-08 19:35:47 +0800

    Added other planets' paths to vite's config to include them in the build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: '.',
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: 'index.html',
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        mercury: resolve(__dirname, 'planets/mercury.html'),
+        venus: resolve(__dirname, 'planets/venus.html'),
+        earth: resolve(__dirname, 'planets/earth.html'),
+        mars: resolve(__dirname, 'planets/mars.html'),
+        jupiter: resolve(__dirname, 'planets/jupiter.html'),
+        saturn: resolve(__dirname, 'planets/saturn.html'),
+        uranus: resolve(__dirname, 'planets/uranus.html'),
+        neptune: resolve(__dirname, 'planets/neptune.html'),
+      },
     },
   },
   server: {
